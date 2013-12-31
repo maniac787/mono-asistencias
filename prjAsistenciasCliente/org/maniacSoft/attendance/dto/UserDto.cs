7a5fcc169beb9a2689286c71543d1da2c67f493e
Roberto <maniac787@gmail.com>
2013-12-31 11:59:31 -0500

    nHibernate ORM

---
+++ prjAsistenciasCliente/org/maniacSoft/attendance/dto/UserDto.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace org.maniacSoft.attendance.dto
+{
+	public class UserDto
+	{
+		public int userId { get; set; }
+		public string usrName { get; set; }
+		public string userPassword { get; set; }
+
+		public UserDto ()
+		{
+		}
+	}
+}
+
